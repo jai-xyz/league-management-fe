d3a452395515790b75489d77c6dfa350cac31deb
jaixyz <jl.ortega01072001@gmail.com>
2025-05-06 21:32:00 +0800

    edit file name

--- src/routes/routesComponents.jsx
+++ src/routes/routesComponents.jsx
@@ -1,36 +1,32 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Login from '../pages/auth/Login'
-import Dashboard from '../pages/admin/dashboard'
-import Test from '../pages/home/Test'
-import Register from '../pages/auth/Register'
-import NotFound from './NotFound' 
-import ProtectedRoutes from './protectedRoutes'
-import AdminLayout from '../layout/admin/AdminLayout'
-import Teams from '../pages/admin/Teams'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/auth/Login";
+import Dashboard from "../pages/admin/dashboard";
+import Test from "../pages/home/test";
+import Register from "../pages/auth/register";
+import NotFound from "./NotFound";
+import ProtectedRoutes from "./protectedRoutes";
+import AdminLayout from "../layout/admin/AdminLayout";
+import Teams from "../pages/admin/teams/teams";
 
 const RoutesComponents = () => {
   return (
     <Routes>
-          {/* // if url is not found, redirect to 404 page */}
-        <Route path="*" element={<NotFound/>}/>
+      {/* // if url is not found, redirect to 404 page */}
+      <Route path="*" element={<NotFound />} />
 
-        <Route path="/" element={<Test/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+      <Route path="/" element={<Test />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-
-        <Route element={<ProtectedRoutes />}>
-        <Route path="/admin" element={<AdminLayout/>}>
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="teams" element={<Teams />} />
         </Route>
       </Route>
-
-        
-        
     </Routes>
-  )
-}
+  );
+};
 
-export default RoutesComponents
+export default RoutesComponents;
